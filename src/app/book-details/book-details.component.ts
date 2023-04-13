822ded5cc1ce23b4cba068da92e587618dff9127
WilliamLinZh <117697083+WilliamLinZh@users.noreply.github.com>
2023-04-14 02:17:31 +0800

    2 files modified

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book, books } from '../books';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.css']
+  styleUrls: ['./book-details.component.css'],
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  book: Book | undefined;
+  books = [...books];
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const bookIdFromRoute = Number(routeParams.get('bookId'));
+
+    this.book = books.find((book) => book.id === bookIdFromRoute);
+  }
 }
