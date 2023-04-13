6619d27a770dc460624569dd6e7805236bc6aee0
WilliamLinZh <117697083+WilliamLinZh@users.noreply.github.com>
2023-04-14 04:28:51 +0800

    2 files modified

--- src/app/book-comments/book-comments.component.ts
+++ src/app/book-comments/book-comments.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-book-comments',
   templateUrl: './book-comments.component.html',
-  styleUrls: ['./book-comments.component.css']
+  styleUrls: ['./book-comments.component.css'],
 })
 export class BookCommentsComponent {
+  commentsForm = this.formBuilder.group({
+    bookName: '',
+    readerName: '',
+    comments: '',
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
+  clearForm() {
+    console.warn('Comments cleared ' + this.commentsForm.value);
+    this.commentsForm.reset();
+  }
+
+  submitComments() {
+    window.alert('Comments submitted!');
+    console.warn('Comments cleared ' + this.commentsForm.value);
+    this.commentsForm.reset();
+  }
 }
